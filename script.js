d711c07df9b9dc409b18aea67cc6e3bc975c9f73
FSG-Pages <fsg.contact01@gmail.com>
2024-11-15 23:38:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function toggleTooltip() {
             const tooltip = document.querySelector('.rekrutacja-btn');
             tooltip.classList.toggle('active');
         }
+        function toggleMenu() {
+            const menu = document.querySelector('.menu');
+            menu.classList.toggle('active');
+        }
         
